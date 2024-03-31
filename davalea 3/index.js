cc721e692780240b3d9564d36a50d9df9616e49d
lizakarsaulidze <115642902+lizakarsaulidze@users.noreply.github.com>
2024-03-31 21:17:22 +0400

    Add files via upload

---
+++ davalea 3/index.js
@@ -0,0 +1,143 @@
+// davaleba 1
+
+
+
+// const div1 = document.getElementById("div1");
+
+
+// div1.addEventListener("toggle", function(){
+//     const bt = document.getElementById("bt");
+//     if(div1.style.display === "none"){
+//         btn.style.display = "block"
+//     }else{
+//         div1.style.display = "none"
+//     }
+// })
+
+
+// function toggle(){
+//     var x = document.getElementById("div1")
+//     if(x.style.display === "none"){
+//         x.style.display = "block"
+//     }else{
+//         x.style.display = "none"
+//     }
+// }
+
+
+// meore davaleba
+
+// function hideShowDiv(val){
+//     if(val==1){
+//         document.getElementById('div').style.display = "none"
+//     }if(val==2){
+//         document.getElementById('div').style.display = "block"
+//     }
+// }
+
+
+// meotxe
+
+// const div1 = document.getElementById("div1");
+
+
+// function toggle(){
+//     var x = document.getElementById("div1")
+//     if(x.style.display === "none"){
+//         x.style.display = "block"
+//     }else{
+//         x.style.display = "none"
+//     }
+// }
+
+
+// shemdegi davaleba changebze
+
+// function changestyle(){
+//     document.getElementById("div").style.backgroundColor="purple";
+//     document.getElementById("div").style.width="200px";
+//     document.getElementById("div").style.height="110px";
+// }
+
+
+// meshvide
+
+// var div = document.getElementById('main');
+
+// var display = 0;
+
+// function hideshow(){
+//     if(display == 1){
+//         div.style.display = "block"
+//         display = 0
+//     }
+//     else{
+//         div.style.display = 'none'
+//         display = 1;
+//     }
+// }
+
+
+
+
+// meqvse
+
+
+// function sizebig(){
+//   document.getElementById("div1").style.width="300px";
+
+// }
+
+// function sizesmall(){
+//     document.getElementById("div1").style.width="100px";
+  
+//   }
+
+
+
+// merve
+
+
+// function backg(){
+//   document.getElementById("main").style.backgroundColor="black";
+
+// }
+
+// function textg(){
+//     document.getElementById("main").style.backgroundColor="white";
+//     document.getElementById("main").style.color="black";
+  
+//   }
+
+
+
+// mecxre
+
+
+// function disable(x){
+//    x.style.display = "none";
+// }
+
+
+// const ck = document.querySelector(".ck");
+
+// ck.addEventListener("click",function(){
+//     ck.style.display = "none"
+// })
+
+
+// meate
+
+// const btn = document.querySelector(".bt");
+
+// btn.addEventListener("click",function(){
+//     btn.style.backgroundColor = "red"
+//     btn.style.color = "black"
+// })
+
+
+ const btn = document.querySelector(".btn");
+
+ btn.addEventListener("toggle",function(){
+    btn.style.backgroundColor = "black";
+})
